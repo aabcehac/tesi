4f79ce6750d9e4e80c75f6e5b40d6a37ebe9081d
Alessandro Cometa <atkdef0@gmail.com>
2024-12-07 22:06:13 +0100

    Line shortening

--- test_sklearn.py
+++ test_sklearn.py
@@ -1,6 +1,7 @@
 from test_sqlite import *
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import *
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsRegressor
@@ -11,12 +12,21 @@ from sklearn.pipeline import make_pipeline
 from sklearn.decomposition import PCA
 from sklearn.metrics import r2_score, mean_squared_error, make_scorer
 from sklearn.tree import plot_tree
+
 Z = ultimate_data_dict
 Z.pop('id')
 y = Z.pop('decile_score')
 X = list(map(list, zip(*Z.values()))) # Transpose the list
-X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.2, random_state=40)
-scalers={'standard': StandardScaler(), 'minmax': MinMaxScaler(), 'quantile': QuantileTransformer()}
+
+X_train, X_test, y_train, y_test= \
+    train_test_split(X,y, test_size=0.2, random_state=40)
+
+scalers={
+    'standard': StandardScaler(),
+    'minmax': MinMaxScaler(),
+    'quantile': QuantileTransformer()
+}
+
 models = {
     'logistic': LogisticRegression(max_iter=1000),
     'svr': SVR(),
@@ -27,32 +37,65 @@ models = {
         ('random_forest', RandomForestRegressor()), 
         ])
 }
-pipelines = {(scaler, estimator): make_pipeline(scalers[scaler], models[estimator]).fit(X_train, y_train) for estimator in models for scaler in scalers}
-scorers = {'mse': make_scorer(mean_squared_error), 'r2': make_scorer(r2_score)}
+
+pipelines = {
+    (s, m): make_pipeline(scalers[s], models[m]).fit(X_train, y_train) 
+        for m in models 
+        for s in scalers
+    }
+
+scorers = {
+    'mse': make_scorer(mean_squared_error),
+    'r2': make_scorer(r2_score)
+}
+
 t = PrettyTable(["Model", "Pre-scaler", "mse_score", "r2_score"])
-t.add_rows([[key[1], key[0], f"{scorers['mse'](pipeline, X_test, y_test):.4f}", f"{scorers['r2'](pipeline, X_test, y_test):.4f}"] for key, pipeline in pipelines.items()])
+t.add_rows([
+    [
+        key[1], 
+        key[0], 
+        f"{scorers['mse'](pipeline, X_test, y_test):.4f}", 
+        f"{scorers['r2'](pipeline, X_test, y_test):.4f}"
+    ] for key, pipeline in pipelines.items()
+])
 print(t)
+
 def PCA_shower(n_components, preprocessor):
     print(f"{preprocessor.__name__}".capitalize())
     pca = PCA(n_components=n_components)
     pca.fit(preprocessor(X_train))
-    t = PrettyTable(["Feature"] + [f"Component {i+1}" for i in range(len(pca.components_))])
-    t.add_row(["Explained variance ratio"] + [f"{pca.explained_variance_ratio_[i]:.4f}" for i in range(len(pca.explained_variance_ratio_))], divider=True)
-    t.add_rows([[feature] + [f"{pca.components_[j][i]:.4f}" for j in range(len(pca.components_))] for i, feature in enumerate(ultimate_data_dict.keys())])
+    t = PrettyTable(
+        ["Feature"] + \
+        [f"Component {i+1}" for i in range(len(pca.components_))]
+    )
+    t.add_row(
+        ["Explained variance ratio"] + \
+        [
+            f"{pca.explained_variance_ratio_[i]:.4f}" \
+            for i in range(len(pca.explained_variance_ratio_))
+        ], divider=True)
+    t.add_rows([[feature] + [f"{pca.components_[j][i]:.4f}"
+                for j in range(len(pca.components_))]
+                for i, feature in enumerate(ultimate_data_dict.keys())])
     return t
+
 print(PCA_shower(5, quantile_transform))
 print(PCA_shower(5, minmax_scale))
 print(PCA_shower(5, scale))
+
 get_rfr = lambda pipeline: pipeline.named_steps['randomforestregressor']
 random_forest=get_rfr(pipelines[('standard', 'random_forest')])
 ########print(get_rfr(pipelines[('standard', 'random_forest')]))
-# Qui esce la foresta della pipeline allenata alla linea 30, se la pipeline ne ha uno.
-tree_scores=[(i, mean_squared_error(y_test, tree.predict(X_test))) for i, tree in enumerate(random_forest.estimators_)]
+tree_scores=[
+    (i, mean_squared_error(y_test, tree.predict(X_test)))
+    for i, tree in enumerate(random_forest.estimators_)
+]
 tree_scores_sorted=sorted(tree_scores, key=lambda x: x[1])
 best_tree_index, best_tree_mse=tree_scores_sorted[0]
 best_tree=random_forest.estimators_[best_tree_index]
 print(f"best tree index: {best_tree_index}")
 print(f"MSE best tree: {best_tree_mse:.4f}")
+
 plt.figure(figsize=(10,5)) 
-plot_tree(best_tree, max_depth=2, feature_names=list(Z.keys()), filled=True)
+plot_tree(best_tree, max_depth=2, feature_names=[*Z.keys()], filled=True)
 plt.show()
--- test_sqlite.py
+++ test_sqlite.py
@@ -4,14 +4,24 @@ from datetime import date
 ### Date handler (conversion to integers)
 
 pot_dates = lambda giorno: (giorno-date(1936,6,24)).days # Ciao, nonna.
-date_converter = lambda item: None if item == None or item == '' else pot_dates(date(*[int(i) for i in (item[0:4], item[5:7], item[8:10])]))
+date_converter = lambda item: \
+    None if item == None or item == '' \
+    else pot_dates(date(*[
+        int(i) for i in (
+            item[0:4],
+            item[5:7],
+            item[8:10]
+        )
+    ]))
 
 ### Utilities and Loading data
 
 file = "data\\compas-analysis\\compas.db"
 con = sqlite3.connect(file)
 cur = con.cursor()
-gen = lambda col: cur.execute(f"SELECT {col} FROM people WHERE is_recid != -1") # Exit class: sql.Cursor
+gen = lambda col: cur.execute(
+    f"SELECT {col} FROM people WHERE is_recid != -1"
+)
 gen_list = lambda gen: [val for tup in gen for val in tup]
 date_int = lambda lst: list(map(date_converter, lst))
 table_gen = lambda data: {col : gen_list(gen(col)) for col in data}
@@ -19,17 +29,37 @@ lindt = [*cur.execute("PRAGMA table_info('people')")]
 columns = [lindt[_][1] for _ in range(len(lindt))]
 data = {col : gen_list(gen(col)) for col in columns}
 
-none_number = lambda caller, data, columns: print(*[f"{caller}" + f"{i}: " + f"Nones: {data[i].count(None)} " + f"Uniques: {len(set(data[i]))} " + f"Total: {len(data[i])}" for i in columns], "", sep="\n")
+none_number = lambda caller, data, columns: \
+print(*[
+    f"{caller}" + \
+    f"{i}: " + \
+    f"Nones: {data[i].count(None)} " + \
+    f"Uniques: {len(set(data[i]))} " + \
+    f"Total: {len(data[i])}"
+    for i in columns
+], "", sep="\n")
 people = ('id', 'first', 'last')
 people_data = table_gen(people)
 
 ### Proiezione algebrica dei dati
 
-short_columns = ('id', 'sex', 'race', 'dob', 'age', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count', 'is_recid', 'decile_score')
+short_columns = (
+    'id',
+    'sex',
+    'race',
+    'dob',
+    'age',
+    'juv_fel_count',
+    'juv_misd_count',
+    'juv_other_count',
+    'priors_count',
+    'is_recid',
+    'decile_score'
+)
 short_data = table_gen(short_columns)
-# none_number('projected_data: ', short_data, short_columns) # projected data structure
+# projected data structure
+# none_number('projected_data: ', short_data, short_columns)
 assert data['is_recid'].count(-1) == 0
-#print("\'decile_score\': ", data['decile_score'].count(-1)) # Output: 15
 
 race_uniques = [*set(short_data['race'])]
 race_list = [f"is_{i.lower()}" for i in race_uniques]
@@ -40,8 +70,10 @@ assert (short_data['id'][i] == i for i in range(len(short_data['id'])))
 short_data['is_male'] = [int(item == 'Male') for item in short_data['sex']]
 short_data['dob_tr'] = [date_converter(item) for item in short_data['dob']]
 for i in range(len(race_uniques)):
-    short_data[race_list[i]] = [int(item == race_uniques[i]) for item in short_data['race']]
-# none_number('transformed_data: ', short_data, transformed) # revised columns
+    short_data[race_list[i]] = \
+    [int(item == race_uniques[i]) for item in short_data['race']]
+# revised columns
+# none_number('transformed_data: ', short_data, transformed)
 kys = [*short_data.keys()]
 #print(kys)
 kys.remove('sex')
